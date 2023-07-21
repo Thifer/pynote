fbb742a5ec8a9cb2a430db9b47aa9592624523a2
NoName <buybackbrain.genetic@gmail.com>
2023-07-21 20:06:03 +0300

    add write and read func

--- Pynote.py
+++ Pynote.py
@@ -4,10 +4,16 @@ import csv
 notehead: str
 note: str
 date: datetime
+filename: str
+filename = 'note.csv'
 
 
 def newnote():
-    pass
+    notehead = input("Введите название заметки: ")
+    note = input("Введите заметку: ")
+    date = datetime.now()
+    fullnote = [notehead, note, date]
+    writenote(fullnote)
 
 
 def changenote():
@@ -18,6 +24,20 @@ def findnote():
     pass
 
 
+def writenote(fullnote: list):
+    notes = readnote()
+    with open(filename, 'w', newline='') as f:
+        writer = csv.writer(f, delimiter=';')
+        writer.writerows(notes)
+        writer.writerow(fullnote)
+
+
+def readnote():
+    with open(filename, 'r', newline='') as f:
+        reader = csv.reader(f, delimiter=';')
+        return list(reader)
+
+
 def menu():
     print("Добро пожаловать в меню заметок")
     print("Пожалуйста, выберете действие")
@@ -25,8 +45,7 @@ def menu():
     print("2.Редактировать заметку")
     print("3.Найти заметку")
     print("Любой другой ввод - для выхода")
-    print()
-    asnwer = int(input())
+    asnwer = int(input("Ответ: "))
     if asnwer == 1:
         newnote()
     elif asnwer == 2:
